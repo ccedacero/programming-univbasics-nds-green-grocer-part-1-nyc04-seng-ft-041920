2f6020f216c0605d874c3a53bb1eadc4c59abec9
Cristian Cedacero <cristian.cedacero@gmail.com>
2020-05-02 22:09:30 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,3 +7,24 @@ end
 nil 
 end
 
+def consolidate_cart(arr)
+cart = [];
+# arr2 = [];
+count = 0 
+current_quant = 0 
+arr.each do |val|
+  item = val
+  item[:count] = 0 
+  cart << item 
+end 
+cart.uniq!
+cart.each do |i|
+  arr.each do |item|
+  if (i[:item] == item[:item])
+    i[:count] +=1
+  end
+end
+end
+cart
+end
+
